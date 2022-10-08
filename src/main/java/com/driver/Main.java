2349ca2178f6d0499ed07f588beb5c7a3f2eb9f8
MyProjects <deshpandepranav47@gmail.com>
2022-10-08 20:30:16 +0530

    made chnages

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
 public class Main {
+    static class Product{
+        public int product(int x,int y){
+            return x*y;
+        }
+        public int product(int x,int y,int z){
+            return x*y*z;
+        }
+        public double product(double x, double y){
+            return x*y;
+        }
+    }
 
+    public static void main(String[] args) {
+        Product p =new Product();
+        int ans1 = p.product(3,4);
+        int ans2 = p.product(2,3,4);
+        double ans3 = p.product(2.4,4.7);
+        System.out.printf(ans1+" "+ans2+" "+ans3);
+    }
 }
